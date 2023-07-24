cee72e54eda3daf429eed0a25a194f923f4626ea
WisnuArdhi28 <wisnuardi128@gmail.com>
2023-07-24 08:11:53 +0700

    import LINARO GCC 11.3.1

---
+++ lib/gcc/arm-linux-gnueabi/11.3.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure CFLAGS='-O2 -fno-stack-protector' CXXFLAGS='-O2 -fno-stack-protector' --prefix=/home/wisnu/build-tools-gcc/arm-linux-gnueabi --target=arm-linux-gnueabi --with-glibc-version=2.37 --with-pkgversion='Arm GNU Toolchain 11.3.rel1 (build-tools-gcc r320-g6cdd090e)' --disable-werror --with-bugurl=https://github.com/krasCGQ/build-tools-gcc/issues  --with-zstd=/home/wisnu/build-tools-gcc/arm-linux-gnueabi --disable-bootstrap --disable-libmpx --disable-libssp --disable-multilib --disable-nls --enable-languages=c,c++";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm10tdmi" }, { "tls", "gnu" } };
